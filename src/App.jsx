2c919c44f82db8bb7c19f1cd09832e4314da2ae5
andrew hulme <andrew_hulme04@hotmail.com>
2024-07-11 07:38:01 +1000

    Implemented client side routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <div>
+        {/* Navigation Links */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
@@ -17,17 +35,7 @@ const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
+      <p>Welcome to the home page!</p>
     </div>
   );
 };
@@ -36,7 +44,7 @@ const About = () => {
   return (
     <div>
       <h1>About</h1>
-      <Link to="/">Home</Link>
+      <p>This is the about page.</p>
     </div>
   );
 };
@@ -44,7 +52,7 @@ const Contact = () => {
   return (
     <div>
       <h1>Contact</h1>
-      <Link to="/">Home</Link>
+      <p>You can contact us at...</p>
     </div>
   );
 };
